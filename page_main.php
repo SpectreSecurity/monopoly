ee03587fbe8a825bde40796b7fb37a6ae4ae6f79
MSA <matsaks@gmail.com>
2013-12-21 19:15:12 +0300

    v6

--- page_main.php
+++ page_main.php
@@ -13,87 +13,18 @@
 
 	</head>
 	<body>
-		<div id="main">
+	<div id="main">
 		<div id="board" style="float: left;">
-			<?php
-			include ('board.php');
+		<?php
+			include ('ppage_board.php');
 		?>
 		</div>
 		<div id="chat" style="float: left;">
-
-	<script>
-	$(function() {
-		$( "#draggablebox" ).draggable();
-		$( "#draggablebox" ).resizable({
-		   stop: function(event, ui) { 
-		   	$("#chats").height(ui.size.height-53);
-		   }
-   		});
-	});
-	</script>
-
-	<div id="draggablebox" class="ui-widget-content">
-	<h3 class="ui-widget-header">Gsession chat</h3>
-<div id="chats_box" style="height:100%;">
-<!--<div id="chats_innerbox" style="height:90%;background-color: #00855c;">-->
-<div id="chats"></div>
-<!--</div>-->
-<!--<div id="chat_edit" style="height:10%;">-->
-<form onsubmit="javascript:sendMsg();return false;">
-	<input type="text" name="text" id="msg" autocomplete="off" />
-</form>
-<!--</div>-->
-</div>
-<script>
-var username = '<?php echo $current_user_name; ?>';
-
-function sendMsg(){
-	if(!username)
-	{
-		username = prompt("Hey there, good looking stranger!  What's your name?", "");
-		if(!username)
-		{
-			return;
-		}
-	}
-
-	var msg = document.getElementById("msg").value;
-	if(!msg)
-	{
-		return;
-	}
-	
-	document.getElementById("chats").innerHTML+=strip('<div class="msgln"><b>'+username+'</b>: '+msg+'<br/></div>');
-	$("#chats").animate({ scrollTop: 2000 }, 'normal');
-
-	$.get('chat.php?msg='+msg+'&user=<?php echo $current_user_name; ?>&room=<?php echo $current_chat_room; ?>', function(data)
-	{
-		document.getElementById("msg").value = '';
-	});
-}
-
-var old = '';
-var source = new EventSource('chat.php?room=<?php echo $current_chat_room; ?>');
-
-source.onmessage = function(e)
-{
-	if(old!=e.data){
-		document.getElementById("chats").innerHTML='<span>'+e.data+'</span>';
-		old = e.data;
-	}
-};
-
-function strip(html)
-{
-	var tmp = document.createElement("DIV");
-	tmp.innerHTML = html;
-	return tmp.textContent||tmp.innerText;
-}
-</script>
+		<?php
+			include ('ppage_chat.php');
+		?>
+		</div>
 	</div>
-		</div>
-		</div>
-		<a href="#" id="colophon" />Menu</a>
-
-	</body>
+	<a href="#" id="colophon" />Menu</a>
+        </body>
 </html>
